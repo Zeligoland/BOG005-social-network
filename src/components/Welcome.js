3ed40f92a3b2f25c0f0760bf950f3afc7d0e3a6e
Tatiana Sanabria Pedraza <tatianasanabr@Invitados-MacBook-Pro.local>
2022-09-17 10:31:15 -0500

    Agrega los primeros estilos CSS y SetAtribute

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,25 +3,35 @@ import { onNavigate } from "../main.js";
 export const Welcome = () => {
   // esta función estará disponible en el main
   const div = document.createElement("div");
+  const nameApp = document.createElement("h1");
   const welcomeTitle = document.createElement("h2");
   const emailInput = document.createElement("input");
+  emailInput.setAttribute("requiered", "")
+  emailInput.setAttribute("placeholder", "Correo electrónico")
+  emailInput.setAttribute("id", "emailLogin")
   const passInput = document.createElement("input");
+  passInput.setAttribute("requiered", "")
+  passInput.setAttribute("placeholder", "Contraseña")
+  passInput.setAttribute("id", "passwordLogin")
   const loginButton = document.createElement("button");
+  loginButton.setAttribute("id", "loginButton1")
   const googleLoginBtn = document.createElement("button");
+  googleLoginBtn.setAttribute("id", "googleLoginBtnG");
   const registerLink = document.createElement("button");
 
-  welcomeTitle.textContent = "Bienvenid@ a tu nueva red de apoyo";
+  nameApp.textContent = "MigroRed";
+  welcomeTitle.textContent = "Bienvenid@ a  la app donde podrás ampliar tu red de apoyo, si eres migrante en Colombia, no conoces muchas personas y deseas conocer más, este es el lugar indicado.";
   emailInput.textContent = "Ingresa tu e-mail"
   passInput.textContent = "Ingresa tu contraseña"
-  loginButton.textContent = "Ingresar - Iniciar Sesión";
-  googleLoginBtn.textContent = "Continua con Google";
+  loginButton.textContent = "Inicia Sesión";
+  googleLoginBtn.textContent = "Sign in with Google";
   registerLink.textContent = "¿No tienes una cuenta? ¡Puedes crearla aquí!";
 
   registerLink.addEventListener("click", () => {
     onNavigate("/register");
   });
   
-  div.append(welcomeTitle, emailInput, passInput, loginButton, googleLoginBtn, registerLink);
+  div.append(nameApp, welcomeTitle, emailInput, passInput, loginButton, googleLoginBtn, registerLink);
 
   return div;
 };
