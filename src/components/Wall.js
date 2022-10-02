262b415356736c15927f7f096d41158cf1895775
Tatiana Sanabria Pedraza <tatianasanabr@Invitados-MacBook-Pro.local>
2022-10-02 11:35:23 -0500

    Agrega configuracion Firestore, ya almacena los post en nuestra db y elimina os mismos mediante el boton delete

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,4 +1,5 @@
 import { onNavigate } from "../main.js";
+import { saveTask, getTasks, onGetTasks, deleteTask, getTask } from "../lib/firest.js";
 
 export const Wall = () => {
 
@@ -21,14 +22,61 @@ export const Wall = () => {
     const postElement = document.createElement("textarea");
     postElement.setAttribute("id", "postContent");
     postElement.setAttribute("placeholder", "¿Que quieres decir/publicar?");
+    postElement.spellcheck = true;
     const postButton = document.createElement("button");
     postButton.setAttribute("id", "postButton1");
     postButton.textContent = "Publicar";
 
     sectionPost.append(postElement, postButton);
 
+    const posters = document.createElement("section");
+    posters.setAttribute("id", "posters");
+    const postComplete = document.createElement("section");
+    postComplete.setAttribute("id", "postComplete");
 
-    div.append(header, sectionPost);
+    posters.append(postComplete);
+
+    backButtonWall.addEventListener("click", () => {
+    onNavigate("/");
+    });
+
+    window.addEventListener("DOMContentLoaded", async () => {
+        
+        onGetTasks((querySnapshot) => {
+            let html = "";
+
+            querySnapshot.forEach((doc) => {
+                const task = doc.data();
+                html += `
+                    <div>
+                        <h3>${task.postElement}</h3>
+                        <button class="btn-delete" data-id="${doc.id}">Borrar</button>
+                    </div>
+                 `;
+             });
+            postComplete.innerHTML = html;
+
+            const btnsDelete = postComplete.querySelectorAll(".btn-delete");
+            
+            btnsDelete.forEach(btn => {
+                btn.addEventListener("click", ({target: {dataset}}) => {
+                    deleteTask(dataset.id);
+                });
+            });
+        });
+    });
+
+    postButton.addEventListener("click", (e) => {
+        e.preventDefault();
+
+       const post =  postElement;
+
+       saveTask(postElement.value);
+
+        postElement.value = ""; 
+    });
+
+    div.append(header, sectionPost, postComplete);
 
     return div;
 };
