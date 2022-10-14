bbd28f99507da4019c9b9daff61dbb5d7d47a5e6
Tatiana Sanabria Pedraza <tatianasanabr@Invitados-MacBook-Pro.local>
2022-10-14 10:13:29 -0500

    Agrega consolidado cambios todas

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -48,103 +48,118 @@ export const Wall = () => {
     let id = "";
     const hour = Date.now();
 
-    onGetTasks((querySnapshot) => {
-        let html = "";
-        let counterLike = 0;
-        querySnapshot.forEach((doc) => {
-            const task = doc.data();
-            const user = auth.currentUser;
-            //Constantes para capturar la fecha
-            const date = new Date();
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1;
-            const day = date.getDate();
-            const hour = date.getHours();
-            const minute = date.getMinutes();
-            let dateNow = month + "/" + day + "/" + year + " " + hour + ":" + minute;
-            /* const hour = Date.now(); */
-            html += `
+
+
+     //window.addEventListener("hashchange",() => { // ¿para que queriamos esto?
+    
+        onGetTasks((querySnapshot) => {
+            let html = "";
+            let counterLike = 0;
+            querySnapshot.forEach((doc) => {
+                const task = doc.data();
+                const user = auth.currentUser;
+                //Constante para capturar la fecha
+                let fecha = new Date();
+                let diaSemana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado"];
+                let mesAnyo = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre",
+                "Noviembre", "Diciembre"];
+                /* console.log(mesAnyo[fecha.getMonth()]);
+                console.log(`${diaSemana[fecha.getDay()]}, ${fecha.getDate()} de ${mesAnyo[fecha.getMonth()]} de ${fecha.getFullYear()}`); */
+                setInterval(() => {
+                    let hora = new Date();
+                    let horaFinal = hora.toLocaleTimeString();
+                   /* console.log(horaFinal);*/
+                }, 1000);
+
+                html += `
                 <div>
                 <section class="postBox">
                 <h2>${task.email}</h2>
                 <h2>${diaSemana[fecha.getDay()]}, ${fecha.getDate()} de ${mesAnyo[fecha.getMonth()]} de ${fecha.getFullYear()}</h2>
                 <br>
-                <button style=" visibility:${task.email === auth.currentUser.email ? "visible" : "hidden"}"class="btn-edit" data-id="${doc.id}"></button>
+                <button style=" visibility:${task.email === auth.currentUser.email?"visible":"hidden"}"class="btn-edit" data-id="${doc.id}"></button>
                 <section class="post">
                 <h3>${task.postElement}</h3>
                 </section>
                 <p class="counter-likes">${task.likes.length}</p>
-                <button class="btn-like" data-id="${doc.id}">like</button>
-                <button style=" display:${task.email === auth.currentUser.email ? "block" : "none"}" class="btn-delete" data-id="${doc.id}">Borrar</button>
+                <button class="btn-like" data-id="${doc.id}"></button>
+                <button style=" display:${task.email === auth.currentUser.email?"block":"none"}" class="btn-delete" data-id="${doc.id}">Borrar</button>
                 </section>
                 </div>
                 `;
-        });
-        postComplete.innerHTML = html;
-
-        const btnsDelete = postComplete.querySelectorAll(".btn-delete");
-
-        const counterLikes = postComplete.querySelectorAll(".counter-likes");
-        const btnLike = postComplete.querySelectorAll(".btn-like");
-
-        btnLike.forEach(btn => {
-            btn.addEventListener("click", ({ target: { dataset } }) => {
-                /* console.log(auth.currentUser.email)
-                 console.log("like",)*/
-                updateTask(dataset.id, {
-                    likes: arrayUnion(auth.currentUser.email)
+            });
+            postComplete.innerHTML = html;
+            
+            const btnsDelete = postComplete.querySelectorAll(".btn-delete");
+            
+            const counterLikes = postComplete.querySelector(".counter-likes");
+            const btnLike = postComplete.querySelectorAll(".btn-like");
+            
+            btnLike.forEach(btn => {
+                btn.addEventListener("click", ({ target: { dataset } }) => {
+                    console.log(auth.currentUser.email)
+                    console.log("like", )
+                    // revisar si en arreglo de likes (task.likes) ya existe el correo
+                    //TIP array.includes 
+                    // Si el email existe debes llar a update task pero con arrayRemove
+                    // Si el email NO existe llmar a update task con arrayUnion
+                    updateTask(dataset.id, {
+                        likes:  arrayUnion(auth.currentUser.email)});
+                   
+                    //doc.data.like incluye user restas uno del arreglo [genesi.com, yeny.com, lina.com] +añadir sergio.com
+                   // firebase metodo sirve aumentar o disminuir
+                    counterLikes.innerHTML=``;
+                    counterLike++;
+                    counterLikes.innerHTML=`${counterLike}`;
+                    
                 });
-                counterLikes.innerHTML = ``;
-                counterLike++;
-                counterLikes.innerHTML = `${counterLike}`;
             });
-        });
 
 
-        btnsDelete.forEach(btn => {
-            btn.addEventListener("click", ({ target: { dataset } }) => {
-                const result = confirm("¿Estás Seguro de borrar esta publicación?");
-                if (result === true) {
-                }
-                deleteTask(dataset.id);
+            btnsDelete.forEach(btn => {
+                btn.addEventListener("click", ({ target: { dataset } }) => {
+                    deleteTask(dataset.id);
+                }); 
             });
-        });
 
-        const btnsEdit = postComplete.querySelectorAll(".btn-edit");
-        const taskForm = document.getElementById("postContent");
+            const btnsEdit = postComplete.querySelectorAll(".btn-edit");
+            const taskForm = document.getElementById("postContent");
 
-        btnsEdit.forEach((btn) => {
-            btn.addEventListener("click", async (e) => {
-                const doc = await getTask(e.target.dataset.id);
-                const task = doc.data();
+            btnsEdit.forEach((btn) => {
+                btn.addEventListener("click", async (e) => {
+                    const doc = await getTask(e.target.dataset.id);
+                    const task = doc.data();
 
-                taskForm.value = task.postElement;
+                    taskForm.value = task.postElement;
 
-                editStatus = true;
-                id = doc.id;
+                    editStatus = true;
+                    id = doc.id;
 
-                postButton.innerText = "Editar";
+                    postButton.innerText = "Editar";
+                })
             })
-        })
-    });
-    //});
+        });
+     //});
 
     postButton.addEventListener("click", (e) => {
         e.preventDefault();
 
         const post = postElement;
+        let fecha = new Date();
 
-        if (!editStatus) {
-            saveTask(postElement.value, auth.currentUser.email);
-        } else {
-            updateTask(id, {
-                postElement: postElement.value
-            });
+
+
+        // saveTask(postElement.value);
+       if (!editStatus) {
+        saveTask(postElement.value,auth.currentUser.email, fecha.value);
+       } else {
+        updateTask(id, {
+            postElement: postElement.value});
             editStatus = false;
 
-        };
+       };
 
-        postElement.value = "";
+        postElement.value = ""; 
         postButton.innerText = "Publicar";
 
     });
